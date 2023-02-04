0be26c054963aaa220e6a039523165bdb79c088c
luisescx <luis.esculapio@live.com>
2023-02-04 12:16:06 -0300

    refactor: changed domain path folders

--- src/domain/product/repository/product-repository.interface.ts
+++ src/domain/product/repository/product-repository.interface.ts
@@ -1,5 +1,5 @@
 import Product from "../../entity/product";
-import RepositoryInterface from "./repository-interface";
+import RepositoryInterface from "../../@shared/repository/repository-interface";
 
 export default interface ProductInterfaceRepository
   extends RepositoryInterface<Product> {}
