4446f1d738ac989750a355aebb0ed8199136ef16
luisescx <luis.esculapio@live.com>
2023-02-05 17:51:28 -0300

    feat: add product factory

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,6 +1,6 @@
 import ProductInterface from "./product.interface";
 
-export default class Product implements ProductInterface {
+export default class ProductB implements ProductInterface {
   private _id: string;
   private _name: string;
   private _price: number;
@@ -12,19 +12,29 @@ export default class Product implements ProductInterface {
     this.validate();
   }
 
-  get id() {
+  get id(): string {
     return this._id;
   }
 
-  get name() {
+  get name(): string {
     return this._name;
   }
 
-  get price() {
-    return this._price;
+  get price(): number {
+    return this._price * 2;
   }
 
-  validate() {
+  changeName(name: string): void {
+    this._name = name;
+    this.validate();
+  }
+
+  changePrice(price: number): void {
+    this._price = price;
+    this.validate();
+  }
+
+  validate(): boolean {
     if (this._id.length === 0) {
       throw new Error("Id is required");
     }
@@ -34,15 +44,6 @@ export default class Product implements ProductInterface {
     if (this._price < 0) {
       throw new Error("Price must be greater than zero");
     }
-  }
-
-  changeName(name: string) {
-    this._name = name;
-    this.validate();
-  }
-
-  changePrice(price: number) {
-    this._price = price;
-    this.validate();
+    return true;
   }
 }
